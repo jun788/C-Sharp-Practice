4ac78bf946189deb52121b6ac46bca3afaa8d93f
Jun Yu <yujun@microsoft.com>
2023-11-26 20:40:30 +0000

    v2.1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -14,7 +14,7 @@ class Progrem()
     }
 
     // This is a function to sort an Int array by Bubble Sort algorithm.
-    static void SortF(int[] arr)
+    static void BubbleSort(int[] arr)
     {
         int temp = 0;
         for (int i = 1; i < arr.Length - 1; i++)
@@ -30,6 +30,37 @@ class Progrem()
             }
         }
     }
+    // This is a function to sort an Int array by QuickSort(Partition recursion) algorithm
+    static void QuickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int PartitionIndex = Partition(arr, low, high);
+            QuickSort(arr, low, PartitionIndex-1);
+            QuickSort(arr, PartitionIndex+1, high);
+        }
+    }
+    static int Partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp1 = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp1;
+        return i + 1;
+    }
+   
+
     //Recurssion function returning a Fibonacci number
     static int Fibonacci(int f)
     {
@@ -57,7 +88,8 @@ class Progrem()
                     s[i] = r.Next(1, 100);
                     Console.Write(s[i] + "  ");
                 }
-                SortF(s);
+                //BubbleSort(s);
+                QuickSort(s, 0, s.Length-1);
                 Console.WriteLine();
                 Console.WriteLine("The array is sorted: ");
                 for (int i = 0; i < s.Length; i++)
@@ -93,7 +125,7 @@ class Progrem()
                     s[i] = r.Next(1, 100);
                     Console.Write(s[i] + "  ");
                 }
-                SortF(s);
+                BubbleSort(s);
                 Console.WriteLine();
                 Console.WriteLine("This is the sorted array: ");
                 for (int i = 0; i < s.Length; i++)
@@ -101,7 +133,7 @@ class Progrem()
                     Console.Write(s[i] + "  ");
                 }
                 Console.WriteLine();
-
+                bool result = false;
                 int mid = 0;
                 int left = 0;
                 int right = s.Length - 1;
@@ -112,6 +144,7 @@ class Progrem()
                     mid = left + (right - left) / 2;
                     if (s[mid] == n)
                     {
+                        result = true;
                         Console.Write("The first position is found at: " + mid);
                         Console.WriteLine(System.Environment.NewLine);
                     }
@@ -120,6 +153,8 @@ class Progrem()
                     else
                         right = mid - 1;
                 }
+                string resultmessage = result ? "":"There is no such number in the array!";
+                Console.WriteLine(resultmessage);
                 Console.ReadKey();
                 MainMenu();
                 Console.WriteLine("Please choose the function to test(0-3):");
